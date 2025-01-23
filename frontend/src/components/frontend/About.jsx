a64a0e4f90c17add7ae80a50071c2c0ea07d8a75
Authoi Chowdhury <authoichy016@gmail.com>
2025-01-23 13:46:49 +0600

    about

--- frontend/src/components/frontend/About.jsx
+++ frontend/src/components/frontend/About.jsx
@@ -6,7 +6,22 @@ const About = () => {
   return (
     <>
     <Header/>
-    <main>About</main>
+    <main>
+    <section className='section-7'>
+               <div className='hero d-flex align-items-center'>
+                  <div className='container'>
+                     <div className='text-left'>
+                        <span>Welcome to Build Craft</span>
+                        <h1>About Us</h1>
+                        <p>We excel at transforming visions into reality<br/> through outstanding craftmanship & Precise.</p>
+                       
+
+                     </div>
+
+                  </div>
+               </div>
+            </section>
+      </main>
     <Footer/>
     </>
   )
