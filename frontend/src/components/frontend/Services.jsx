252daa45673aa263a17535715cd7465fa61e6dfa
Authoi Chowdhury <authoichy016@gmail.com>
2025-01-23 21:54:15 +0600

    Services

---
+++ frontend/src/components/frontend/Services.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+import Header from '../common/Header';
+
+import Footer from '../common/Footer';
+
+const Services = () => {
+  return (
+    <>
+      <Header />
+      <div>Services</div>
+      <Footer />
+    </>
+  );
+};
+
+export default Services;
+
