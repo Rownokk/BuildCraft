82acad80398dae097187e0f2d697f34f461b9405
Authoi Chowdhury <authoichy016@gmail.com>
2025-02-28 02:34:38 +0600

    remove static

--- frontend/src/components/common/LatestServices.jsx
+++ frontend/src/components/common/LatestServices.jsx
@@ -60,69 +60,6 @@ const LatestServices =() =>{
             }
 
              
-              <div className='col-md-3 col-lg-3'>
-                 <div className='item'>
-                    <div className='service-image'>
-
-                       <img src={ServiceImg} alt="" className='w-100' />
-                    </div>
-                    <div className='service-body'>
-                       <div className='service-title'>
-                          <h3>Civil Construction</h3>
-                          <div className='service-contain'>
-                             <p>
-                             Handling large-scale infrastructure projects like roads, bridges, and public facilities, ensuring safety, sustainability, and compliance with engineering standards.
-                             </p>
-                          </div>
-                          <a href='#' className='btn btn-primary small'>Read More</a>
-                       </div>
-
-                    </div>
-                 </div>
-
-              </div>
-              <div className='col-md-3 col-lg-3'>
-                 <div className='item'>
-                    <div className='service-image'>
-
-                       <img src={ServiceImg} alt="" className='w-100' />
-                    </div>
-                    <div className='service-body'>
-                       <div className='service-title'>
-                          <h3>Residential Construction</h3>
-                          <div className='service-contain'>
-                             <p>
-                                Focused on building and renovating homes, ensuring comfort, durability, and modern design with efficient project management and resource planning.
-                             </p>
-                          </div>
-                          <a href='#' className='btn btn-primary small'>Read More</a>
-                       </div>
-
-                    </div>
-                 </div>
-
-              </div>
-              <div className='col-md-3 col-lg-3'>
-                 <div className='item'>
-                    <div className='service-image'>
-
-                       <img src={ServiceImg} alt="" className='w-100' />
-                    </div>
-                    <div className='service-body'>
-                       <div className='service-title'>
-                          <h3>Corporate Construction</h3>
-                          <div className='service-contain'>
-                             <p>
-                             Developing office spaces, commercial buildings, and business hubs with innovative designs, optimized layouts, and advanced infrastructure to enhance productivity.
-                             </p>
-                          </div>
-                          <a href='#' className='btn btn-primary small'>Read More</a>
-                       </div>
-
-                    </div>
-                 </div>
-
-              </div>
            </div>
         </div> 
         </section>
