11e1ec7628535143860c9fbfeed580c54ee1ae0e
Rownok Jahan Mowmita <151154578+Rownokk@users.noreply.github.com>
2025-03-07 00:10:38 +0600

    memberController ,#52

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -8,10 +8,12 @@ use App\Http\Controllers\admin\ProjectController;
 use App\Http\Controllers\admin\TempImageController;
 use App\Http\Controllers\admin\MemberController;
 use App\Http\Controllers\front\ServiceController as FrontServiceController;
+use App\Http\Controllers\admin\MemberController as FrontMemberController;
 
 Route::post('authenticate', [AuthenticationController::class, 'authenticate']);
 Route::get('get-services', [FrontServiceController    ::class, 'index']);
 Route::get('get-latest-services', [FrontServiceController    ::class, 'latestServices']);
+Route::get('get-members', [FrontMemberController    ::class, 'index']);
 
 Route::middleware(['auth:sanctum'])->group(function () {
     Route::get('dashboard', [DashboardController::class, 'index']);
