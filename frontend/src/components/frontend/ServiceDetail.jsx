1233343116ac8be030c2d0748b2d6e288e4e5417
Rownok Jahan Mowmita <151154578+Rownokk@users.noreply.github.com>
2025-03-07 15:02:57 +0600

    Detail title , show details #56

--- frontend/src/components/frontend/ServiceDetail.jsx
+++ frontend/src/components/frontend/ServiceDetail.jsx
@@ -1,17 +1,37 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Header from '../common/Header'
 import Footer from '../common/Footer'
 import Hero from '../common/Hero';
+import { useParams } from 'react-router-dom';
+import { apiUrl } from '../common/http';
 
 const ServiceDetail = () => {
+
+    const params = useParams();
+    const [service,setService] = useState([]);
+
+    const fetchService = async () => {
+        const res =await fetch(`${apiUrl}get-service/${params.id}`,{
+           method: 'GET'
+        });
+
+        const result =await res.json();
+        setService(result.data);
+        console.log(result);
+    }
+  
+    useEffect(() => {
+       fetchService();
+    },[])
+
   return (
     <>
         <Header/>
         <main>
         <Hero 
           preHeading="Quality. Integrity. Value" 
-          heading="About Us"
-          text="We excel at transforming visions into reality <br/> through outstanding craftsmanship & Precision."
+          heading={`${service.title}`}
+          text=" "
         />
         </main>
         
